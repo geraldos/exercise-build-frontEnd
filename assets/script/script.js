02834b220e340293ebc0b9f7b533895e7168d852
geraldos <sepdwijaya18@gmail.com>
2020-06-24 15:30:01 +0700

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,51 @@
+jQuery(function ($) {
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() >= 200) {
+            $('.navbar').addClass('fixed-top');
+        } else if ($(this).scrollTop() == 0) {
+            $('.navbar').removeClass('fixed-top');
+        }
+    });
+
+    function adjustNav() {
+        var winWidth = $(window).width(),
+            dropdown = $('.dropdown'),
+            dropdownMenu = $('.dropdown-menu');
+
+        if (winWidth >= 768) {
+            dropdown.on('mouseenter', function () {
+                $(this).addClass('show')
+                    .children(dropdownMenu).addClass('show');
+            });
+
+            dropdown.on('mouseleave', function () {
+                $(this).removeClass('show')
+                    .children(dropdownMenu).removeClass('show');
+            });
+        } else {
+            dropdown.off('mouseenter mouseleave');
+        }
+    }
+
+    $(window).on('resize', adjustNav);
+
+    adjustNav();
+});
+
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 71)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+})(jQuery)
